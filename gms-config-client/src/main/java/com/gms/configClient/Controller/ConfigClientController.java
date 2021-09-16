946302a02eff2096cc71df9dd3a3bca3b846225a
283872743@qq.com <ghx19880213>
2021-09-16 14:42:06 +0800

    update 8

---
+++ gms-config-client/src/main/java/com/gms/configClient/Controller/ConfigClientController.java
@@ -0,0 +1,20 @@
+package com.gms.configClient.Controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${profile}")
+    private String profile;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return this.profile;
+    }
+}
